0d817708d49e41ca5442a9e1430e319854ff839c
Sawom <asawom250@gmail.com>
2026-01-31 22:33:25 +0600

    eye icon show/ hide password featured added

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,6 +5,7 @@ import { registerUser } from "@/utils/actions/registerUser";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 export type UserData = {
@@ -14,6 +15,13 @@ export type UserData = {
 };
 
 const RegisterPage = () => {
+  // toggle show password
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   const {
     register,
     handleSubmit,
@@ -69,7 +77,7 @@ const RegisterPage = () => {
                 required
               />
             </div>
-
+            {/* email */}
             <div className="mb-4">
               <label className="block text-gray-700 font-medium mb-2">
                 Email
@@ -82,18 +90,66 @@ const RegisterPage = () => {
                 required
               />
             </div>
-
+            {/* password */}
             <div className="mb-6">
               <label className="block text-gray-700 font-medium mb-2">
                 Password
               </label>
-              <input
-                {...register("password")}
-                type="password"
-                placeholder="Password"
-                className="w-full p-3 border border-gray-300 rounded "
-                required
-              />
+
+              <div className="relative mt-1">
+                <input
+                  {...register("password")}
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  className="w-full p-3 border border-gray-300 rounded "
+                  required
+                />
+                {/* eye button */}
+                <button
+                  type="button"
+                  onClick={togglePasswordVisibility}
+                  className="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-500 hover:text-gray-700"
+                >
+                  {showPassword ? (
+                    // Eye-off Icon
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-5 h-5"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                      />
+                    </svg>
+                  ) : (
+                    // Eye open Icon
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-5 h-5"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.036 12.322a1.012 1.012 0 010-.644C3.412 7.943 7.244 4.5 12 4.5c4.756 0 8.773 3.162 10.065 7.498a1.01 1.01 0 010 .644C20.588 16.057 16.756 19.5 12 19.5c-4.756 0-8.773-3.162-10.065-7.498z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                    </svg>
+                  )}
+                </button>
+              </div>
             </div>
 
             <div className="mb-4">
